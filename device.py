6ef999afb1d4283de94c72e463c95f7ca2f6130a
David Kensiski <David@Kensiski.org>
2021-03-16 06:49:48 -0700

    changed dict names

--- device.py
+++ device.py
@@ -6,8 +6,8 @@ from gpiozero import DigitalInputDevice, DigitalOutputDevice
 
 class Sensor:
     sensors = []    # array of all sensor instances (in order of creation for status printout)
-    sensor = {}     # dict of all sensors by name
-    pin = {}        # dict of all sensors by pin
+    by_name = {}    # dict of all sensors by name
+    by_pin = {}     # dict of all sensors by pin
     names = []      # array of sensor names (in order of creation for status printout)
 
     def __init__(self, pin, name, pull_up=False, active_state=True, bounce_time=0.1):
@@ -15,8 +15,8 @@ class Sensor:
         self.device = DigitalInputDevice(pin=pin, pull_up=pull_up, bounce_time=bounce_time)
         self.name = name
         Sensor.sensors.append(self)
-        Sensor.sensor[name] = self
-        Sensor.pin[pin] = self
+        Sensor.by_name[name] = self
+        Sensor.by_pin[pin] = self
         Sensor.names.append(name)
 
     def is_active(self):
@@ -25,9 +25,9 @@ class Sensor:
 
 
 class Control:
-    controls = []    # array of all sensor instances (in order of creation for status printout)
-    control = {}     # dict of all controls by name
-    pin = {}        # dict of all controls by pin
+    controls = []   # array of all sensor instances (in order of creation for status printout)
+    by_name = {}    # dict of all controls by name
+    by_pin = {}     # dict of all controls by pin
     names = []      # array of control names (in order of creation for status printout)
 
     def __init__(self, pin, name, active_high=True, initial_value=False):
@@ -35,8 +35,8 @@ class Control:
         self.device = DigitalOutputDevice(pin=pin, active_high=active_high, initial_value=initial_value)
         self.name = name
         Control.controls.append(self)
-        Control.control[name] = self
-        Control.pin[pin] = self
+        Control.by_name[name] = self
+        Control.by_pin[pin] = self
         Control.names.append(name)
 
     def is_active(self):
