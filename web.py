8799eb679e9c2c74980beeefeb1033989dd72e14
David Kensiski <David@Kensiski.org>
2021-04-06 08:32:49 -0700

    checkpoint: working with limited standalone functionality

--- web.py
+++ web.py
@@ -1,44 +1,74 @@
-#!/usr/bin/python3
+#!/usr/bin/python3 URL callbacks
 #
 # Flask based web frontend for trax
 
-from flask import Flask, render_template
-app = Flask(__name__)
+# One shot:
+# FLASK_APP=web.py flask run --host=0.0.0.0 &
+# Auto-reloading:
+# FLASK_ENV=development FLASK_APP=web.py flask run --host=0.0.0.0 &
 
+
+import datetime
+import json
 import queue
-from time import sleep
 import signal
+import sys
+import threading
 
-@app.route('/')
-def top():
-    return render_template('trax.html')
+import flask
 
-# Cobbled from https://maxhalford.github.io/blog/flask-sse-no-deps/
+# Message queue cobbled from https://maxhalford.github.io/blog/flask-sse-no-deps/
+
 class MessageAnnouncer:
+    """Implement a queue for messages to be sent to connected browsers"""
 
     def __init__(self):
         self.listeners = []
 
     def listen(self):
-        q = queue.Queue(maxsize=5)
+        """Consumers of the queue listen() for queue updates"""
+        q = queue.Queue(maxsize=10)
         self.listeners.append(q)
+        colorUp()
         return q
 
     def announce(self, msg):
+        """Producers of queue messages announce() them here"""
         for i in reversed(range(len(self.listeners))):
             try:
                 self.listeners[i].put_nowait(msg)
             except queue.Full:
                 del self.listeners[i]
 
-announcer = MessageAnnouncer()
-
 def format_sse(data: str, event=None) -> str:
+    """Format `data` with optional `event` as a Server Side Event"""
+    # Consider putting this in announce() above?
     msg = f'data: {data}\n\n'
     if event is not None:
         msg = f'event: {event}\n{msg}'
     return msg
 
+
+# Silly loop to send something every second
+def perSecond():
+    msg = json.dumps( { 'tag':'notice', 'data':datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") } )
+    fmsg = format_sse(data=msg, event='innerHTML')
+    #print(fmsg)
+    announcer.announce(fmsg)
+    threading.Timer(1.0, perSecond).start()  # Redispatch self
+
+
+#
+# Register URL callbacks
+#
+
+app = flask.Flask(__name__)
+
+@app.route('/')
+def top():
+    return flask.render_template('trax.html')
+
+# Sample SSE from the example; delete
 @app.route('/ping')
 def ping():
     msg = format_sse(data='pong')
@@ -47,8 +77,13 @@ def ping():
 
 @app.route('/connect', methods=['GET'])
 def connect():
+    """
+    This function starts in a different thread (I think) for each browser that connects,
+    listens on the `announcer` queue and sends any messages that are posted on it.
+    """
 
     def stream():
+        """Loop on the `announcer` queue and "yields" any message posted to the queue."""
         messages = announcer.listen()  # returns a queue.Queue
         while True:
             msg = messages.get()  # blocks until a new message arrives
@@ -58,20 +93,24 @@ def connect():
 
 
 
-notice = "Connecting to T-Rax...<br/>\
-1 3 5 7 9 1 3 5 7 9 1 <br/>\
-&nbsp;2 4 6 8 0 2 4 6 8 0 2<br/>"
+# Instantiate the message queue
+announcer = MessageAnnouncer()
 
 back_on_green = "background-color:#00FF00; color:black"
 yellow_on_red = "background-color:#FF0800; color:yellow"
 
-announcer.announce(msg="Connected")
+def colorUp():
+    print("colorUp")
+    announcer.announce(format_sse(event='innerHTML', data=json.dumps( { 'tag':'notice', 'data':'Connected!' } )))
+    announcer.announce(format_sse(event='bgcolor', data=json.dumps( { 'tag':'roof_position', 'data':'#00FF00' } )))
+    announcer.announce(format_sse(event='fgcolor', data=json.dumps( { 'tag':'roof_position', 'data':'black' } )))
+    announcer.announce(format_sse(event='innerHTML', data=json.dumps( { 'tag':'roof_position', 'data':'OPEN' } )))
 
-def alarmHandler(signum,frame):
-    announcer.announce(msg="Connected")
+    announcer.announce(format_sse(event='bgcolor', data=json.dumps( { 'tag':'mount_position', 'data':'#FF0800' } )))
+    announcer.announce(format_sse(event='fgcolor', data=json.dumps( { 'tag':'mount_position', 'data':'yellow' } )))
+    announcer.announce(format_sse(event='innerHTML', data=json.dumps( { 'tag':'mount_position', 'data':'PARKED' } )))
 
-signal.signal(signal.SIGALRM, alarmHandler)
-signal.alarm(heartInterval)
-while True:
-    signal.pause()
-    signal.alarm(1)
+#threading.Timer(2.4, colorUp).start()  # Set colors
+
+# Start it all up
+perSecond()
