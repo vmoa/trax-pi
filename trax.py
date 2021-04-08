6bb325eeafa20f6e9aa60caa8bf09df427845003
David Kensiski <David@Kensiski.org>
2021-04-07 23:09:45 -0700

    moved flask code into trax.py

---
+++ trax.py
@@ -0,0 +1,141 @@
+#!/usr/bin/python3
+#
+# T-Rax roof controller for the Raspberry Pi
+#
+# This is a roll-off roof controller specifically designed and built for
+# Robert Ferguson Observatory (https://rfo.org) but the code will hopefully
+# be generic enough that it can be adapted for other roll-off roofs.
+# See README.md for details.
+
+import argparse
+import datetime
+from gpiozero import DigitalInputDevice, DigitalOutputDevice
+import logging
+import os
+import signal
+import sys
+import threading
+
+import device
+import test_mode
+import util
+import web
+
+import flask
+
+version = 'v0.9'        # T-Rax version
+heartInterval = 1       # Seconds between heartbeats (drives SIGALRM)
+statusInterval = 60     # Seconds between status updates without input changes
+
+# Hard coded GPIO setup -- should these be in a dict?
+class TraxGpio:
+    def __init__(self):
+        self.weatherOk = device.Sensor(pin=4, name='wx');
+        self.bldgPowerIn = device.Sensor(pin=17, name='bldg');
+        self.mountPowerIn = device.Sensor(pin=22, name='mntin');
+        self.roofPowerIn = device.Sensor(pin=27, name='roofin');
+        self.mountParked = device.Sensor(pin=25, name='park');
+        self.roofOpen = device.Sensor(pin=23, name='open');
+        self.roofClosed = device.Sensor(pin=24, name='close');
+
+        self.heartLed = device.Control(pin=13, name='heart')
+        self.mountPowerOut = device.Control(pin=6, name='mntout', active_high=False)
+        self.roofPowerOut = device.Control(pin=5, name='roofout', active_high=False)
+        self.laserPowerOut = device.Control(pin=16, name='laser')
+        self.fobOutput = device.Control(pin=26, name='fob')
+
+def beatHeart(output=0, step=0):
+    if (step == 0):
+        output.on()
+        threading.Timer(0.1, beatHeart, [output,1]).start()
+    elif (step == 1):
+        output.off()
+        threading.Timer(0.05, beatHeart, [output,2]).start()
+    elif (step == 2):
+        output.on()
+        threading.Timer(0.1, beatHeart, [output,3]).start()
+    elif (step == 3):
+        output.off()
+    else:
+        logging.error("WTF? beatHeart() called with step %".format(step))
+
+def perSecond():
+    browser.send(id='notice', type='innerHTML', data=datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
+    if (int(datetime.datetime.now().second) % 2 == 0):
+        beatHeart(gpio.heartLed.device)
+    if (int(datetime.datetime.now().second) % statusInterval == 0):
+        logging.info(device.printStatus())
+    threading.Timer(1.0, perSecond).start()  # Redispatch self
+
+
+#def main():
+
+default_logfile = '/var/log/trax.log'
+
+#parser = argparse.ArgumentParser(description='T-Rax roof controller.')
+#parser.add_argument('--test-mode', dest='test_mode', action='store_true', help='enter test mode')
+#parser.add_argument('--log-file', '-L', dest='logfile', action='store', help='log filename (default {})'.format(default_logfile))
+#args = parser.parse_args()
+
+loggingConfig = dict(format = "%(asctime)s [%(levelname)s] %(message)s", datefmt = "%Y-%m-%d %H:%M:%S", level = logging.INFO)
+#if (sys.stdin.isatty() and not args.logfile):
+if (sys.stdin.isatty()):
+    print("TTY detected; logging to stdout")
+else:
+    loggingConfig['filename'] = default_logfile;
+logging.basicConfig(**loggingConfig)
+logging.info("Initializing T-Rax for the Raspberry Pi {}".format(version))
+
+global gpio
+gpio = TraxGpio()
+
+logging.info(device.printStatus())
+
+#if (args.test_mode):
+#    test = test_mode.TestMode()
+
+# Loop forever
+#while True:
+#    signal.pause()
+
+#
+# Register URL callbacks
+#
+
+# Instantiate the message queue
+
+color = { 'black':'black', 'yellow':'yellow', 'green':'#00FF00', 'red':'#FF0800' }
+
+def colorUp():
+    print("colorUp")
+    browser.send(id='notice', type='innerHTML', data='Connected!')
+    browser.send(id='roof_position', type='bgcolor', data=color['green'])
+    browser.send(id='roof_position', type='fgcolor', data=color['black'])
+    browser.send(id='roof_position', type='innerHTML', data='OPEN')
+
+    browser.send(id='mount_position', type='bgcolor', data=color['red'])
+    browser.send(id='mount_position', type='fgcolor', data=color['yellow'])
+    browser.send(id='mount_position', type='innerHTML', data='PARKED')
+
+browser = web.MessageAnnouncer(welcomeFunc = colorUp)
+
+app = flask.Flask(__name__)
+
+@app.route('/')
+def top():
+    return flask.render_template('trax.html')
+
+@app.route('/connect', methods=['GET'])
+def connect():
+    """
+    This function starts in a different thread (I think) for each browser that connects,
+    listens on the `browser` queue and sends any messages that are posted to it.
+    """
+    return flask.Response(browser.stream(), mimetype='text/event-stream')
+
+# Start the self perpetuating per second timer
+perSecond()
+
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', 5000)
--- web.py
+++ web.py
@@ -22,20 +22,21 @@ import flask
 class MessageAnnouncer:
     """Implement a queue for messages to be sent to connected browsers"""
 
-    def __init__(self):
+    def __init__(self, welcomeFunc):
         self.listeners = []
+        self.welcomeFunc = welcomeFunc
 
     def listen(self):
         """Consumers of the queue listen() for queue updates"""
         q = queue.Queue(maxsize=32)
         self.listeners.append(q)
-        threading.Timer(1.0, colorUp).start()  # Dispatch
+        threading.Timer(1.0, self.welcomeFunc).start()  # Dispatch our welcome function (we need a better name)
         return q
 
     def send(self, msg='', id='', type='', data=''):
         """Producers of queue messages send() them here"""
         if (not msg):
-            msg = format_sse(data=json.dumps( { 'id':id, 'data':data } ), event=type)
+            msg = self.format_sse(data=json.dumps( { 'id':id, 'data':data } ), event=type)
         for i in reversed(range(len(self.listeners))):
             try:
                 self.listeners[i].put_nowait(msg)
@@ -50,68 +51,11 @@ class MessageAnnouncer:
             # print("Yielding", msg)
             yield msg
 
-def format_sse(data: str, event=None) -> str:
-    """Format `data` with optional `event` as a Server Side Event"""
-    # Consider putting this in send() above?
-    msg = f'data: {data}\n\n'
-    if event is not None:
-        msg = f'event: {event}\n{msg}'
-    return msg
+    def format_sse(self, data: str, event=None) -> str:
+        """Format `data` with optional `event` as a Server Side Event"""
+        # Consider putting this in send() above?
+        msg = f'data: {data}\n\n'
+        if event is not None:
+            msg = f'event: {event}\n{msg}'
+        return msg
 
-
-# Silly loop to send something every second
-def perSecond():
-    browser.send(id='notice', type='innerHTML', data=datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
-    threading.Timer(1.0, perSecond).start()  # Redispatch self
-
-def fizzbin(id, type, data):
-    msg = format_sse(data=json.dumps( { 'id':id, 'data':data } ), event=type)
-    return msg
-
-#
-# Register URL callbacks
-#
-
-# Instantiate the message queue
-browser = MessageAnnouncer()
-
-app = flask.Flask(__name__)
-
-@app.route('/')
-def top():
-    return flask.render_template('trax.html')
-
-# Sample SSE from the example; delete
-@app.route('/ping')
-def ping():
-    msg = format_sse(data='pong')
-    browser.send(msg=msg)
-    return {}, 200
-
-@app.route('/connect', methods=['GET'])
-def connect():
-    """
-    This function starts in a different thread (I think) for each browser that connects,
-    listens on the `browser` queue and sends any messages that are posted to it.
-    """
-    return flask.Response(browser.stream(), mimetype='text/event-stream')
-
-
-
-back_on_green = "background-color:#00FF00; color:black"
-yellow_on_red = "background-color:#FF0800; color:yellow"
-color = { 'black':'black', 'yellow':'yellow', 'green':'#00FF00', 'red':'#FF0800' }
-
-def colorUp():
-    print("colorUp")
-    browser.send(id='notice', type='innerHTML', data='Connected!')
-    browser.send(id='roof_position', type='bgcolor', data=color['green'])
-    browser.send(id='roof_position', type='fgcolor', data=color['black'])
-    browser.send(id='roof_position', type='innerHTML', data='OPEN')
-
-    browser.send(id='mount_position', type='bgcolor', data=color['red'])
-    browser.send(id='mount_position', type='fgcolor', data=color['yellow'])
-    browser.send(id='mount_position', type='innerHTML', data='PARKED')
-
-# Start it all up
-perSecond()
